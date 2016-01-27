499c6acd9368e38eaca137896a5b957f2727a85f
Amihai Shapira <samihi@gmail.com>
2016-01-27 06:42:02 +0000

    improved tests and backtrace

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,12 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
+  # order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
